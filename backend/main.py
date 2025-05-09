5a0e5889ffddc37e964482e77250b12d37ab3e47
Indrasena-913 <sindrasenagowdu_jtbb2@jtdfoundation.org>
2025-05-09 23:48:43 +0530

    final touches

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from unicodedata import category
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 import models
 import auth
 import products
@@ -10,6 +12,14 @@ import orders
 from database import engine
 
 app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth.router)
 app.include_router(products.router)
 app.include_router(cart.router)
