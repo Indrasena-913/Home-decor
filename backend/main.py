4bfc3d5b1d98bdcd68f701d10d3d143334fe0e62
Indrasena-913 <sindrasenagowdu_jtbb2@jtdfoundation.org>
2025-05-10 10:22:35 +0530

    cors error

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ app=FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://127.0.0.1:5501"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
